28ed69520124095c6010ab4c80c29460aef1f078
abhay-byte <abhay02delhi@gmail.com>
2025-04-06 08:54:10 +0530

    first changes

---
+++ CB/LEC4/1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  cout<< "hello world" << endl;
+  cout << INTMAX_MAX << endl;
+  cout << INTMAX_MIN << endl;
+  return 0;
+
+}
