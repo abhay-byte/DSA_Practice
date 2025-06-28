d60c4f00170ebd91eb8fc45701bde2b012fb53b6
abhay-byte <abhay02delhi@gmail.com>
2025-06-28 11:20:19 +0530

    lec17

---
+++ CB/LEC17/Temp.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+
+bool isPalindrome(char str[])
+{
+    int i = 0;
+    int j = strlen(str) - 1;
+
+    while (i < j)
+    {
+        if(str[i] == str[j])
+        {
+            i++;
+            j++;
+        }
+        else{
+            return false;
+        }
+    }
+
+    return true;
+
+}
+
+int main()
+{
+    char str[] = "racecar";
+
+    cout << isPalindrome(str);
+}
