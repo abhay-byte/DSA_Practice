c7dd824204aeedc1d106cb9c4f66fefb23fcd9b3
abhay-byte <abhay02delhi@gmail.com>
2025-07-02 22:56:22 +0530

    done lec17 hw
    Co-authored-by: REVA <abhay.byte02@gmail.com>

--- CB/LEC17/12_sort_custom_comparator.cpp
+++ CB/LEC17/12_sort_custom_comparator.cpp
@@ -5,22 +5,18 @@
 #include<iomanip>
 using namespace std;
 
-bool cmp(string a, string b)
+bool cmp(const string& a, const string& b)
 {
-    if(a.size() < b.size())
-    {
-        return true;
-    }
-    return false;
+    return a + b > b + a;
 }
 
 int main()
 {
     vector<string> v;
-    v.push_back("aaaa");
-    v.push_back("z");
-    v.push_back("yy");
-    v.push_back("ccc");
+    v.push_back("129");
+    v.push_back("97");
+    v.push_back("998");
+    v.push_back("67");
 
     sort(v.begin(),v.end(), cmp);
     for(string a : v)
---
+++ CB/LEC17/13_form_largest_number.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+
+        }
+    }
+};
