ea11f476e6ee978f17c24b0cf9459cb3d9c1863b
abhay-byte <abhay02delhi@gmail.com>
2025-07-13 23:25:47 +0530

    lec22 done

--- CB/LEC22/6-power.cpp
+++ CB/LEC22/6-power.cpp
@@ -3,10 +3,10 @@
 #include <algorithm>
 using namespace std;
 
-int f(int x , int y)
+int f(int x, int y)
 {
     // create a base case, cases which are invalid, and we know the answers.
-    if(y == 0)
+    if (y == 0)
     {
         return 1;
     }
@@ -14,14 +14,15 @@ int f(int x , int y)
     // recursive case
     // f(n) = find n!
     // 1. ask your friend to find n-1th fibonacci no.
-    int a = f(x,y-1);
+    int a = f(x, y - 1);
 
     return a * x;
 }
 
 int main()
 {
-    int x = 2; int y = 16;
+    int x = 2;
+    int y = 16;
 
-    cout << f(x,y) << endl;
+    cout << f(x, y) << endl;
 }
