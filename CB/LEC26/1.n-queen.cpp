cad4986eff3af7a811c759f38e11973e6f5fd8b5
abhay-byte <abhay02delhi@gmail.com>
2025-08-02 22:30:06 +0530

    n-queen updated

--- CB/LEC26/1.n-queen.cpp
+++ CB/LEC26/1.n-queen.cpp
@@ -1,113 +1,145 @@
+/*
+
+https://leetcode.com/problems/n-queens/
+
+N Queens Problem
+
+The problem is to place 'n' queens on an 'n x n' chessboard, so that no two queens are attacking
+each other, this means that no two queens are in the same row, the same column, or the same diagonal.
+
+Example
+	Input : N = 4
+	Output:
+			_ Q _ _	     _ _ Q _
+	        _ _ _ Q      Q _ _ _
+	        Q _ _ _      _ _ _ Q
+	        _ _ Q _      _ Q _ _
+
+	        [1, 3, 0, 2] [2, 0, 3, 1]
+
+Constraint:
+	1<=n<=10
+
+*/
+
 #include<iostream>
 #include<vector>
 #include<string>
+
 using namespace std;
 
 class Solution {
 public:
 
-    bool isValid(int r, int n, vector<string> board,int j)
-    {
-        // check if we can place the Qr in the jth column of row r;
+	bool isvalid(int r, int n, vector<string>& board, int j) {
 
-        // 1. check in the jth column if there if any previously placed queen Qi where 0<=i<=r-1
+		// check if you can place Qr in the jth column of row r ?
 
-        for(int i = r - 1; i >= 0 ; i--)
-        {
-            if(board[i][j] == 'Q')
-            {
-                //there is prev placed queen in the jth column of the board therefore u cannot place Qr in the jth column of row
-                return false;
-            }
-        }
+		// 1. check in the jth column if there is any previously
+		// placed queen Qi where 0<=i<=r-1
 
-        // 2. check in the right diagonal of the jth column if there is any previously placed queen Qi where 0<=i<=r-1
+		for (int i = r - 1; i >= 0; i--) {
+			if (board[i][j] == 'Q') {
+				// there is already a previously placed queen in the jth column
+				// of the board therefore you cannot place Qr in the jth column
+				// of row r
+				return false;
+			}
+		}
 
-        for(int i = r - 1, step = 1; i >= 0 && step+j < n ; i--,step++)
-        {
-            if(board[i][j+step] == 'Q')
-            {
-                //there is prev placed queen along the right diagonal in the jth column of the board therefore u cannot place Qr in the jth column of row
-                return false;
-            }
-        }        
 
-        // 3. check in the left diagonal of the jth column if there is any previously placed queen Qi where 0<=i<=r-1
+		// 2. check in the right diagonal of the jth column if there is
+		// any previously placed queen Qi where 0<=i<=r-1
 
-        for(int i = r - 1, step = 1; i >= 0 && step-j >= 0 ; i--,step++)
-        {
-            if(board[i][j-step] == 'Q')
-            {
-                //there is prev placed queen along the left diagonal in the jth column of the board therefore u cannot place Qr in the jth column of row
-                return false;
-            }
-        }   
+		for (int i = r - 1, step = 1; i >= 0 and j + step < n; i--, step++) {
+			if (board[i][j + step] == 'Q') {
+				// you've found a previously placed queen along the right diag
+				// of the jth column so you cannot place queen r in the jth col
+				// of row r
+				return false;
+			}
+		}
 
-        return true;
+		// 3. check in the left diagonal of the jth column if there is
+		// any previously placed queen Qi where 0<=i<=r-1
 
-    }
+		for (int i = r - 1, step = 1; i >= 0 and j - step >= 0; i--, step++) {
+			if (board[i][j - step] == 'Q') {
+				// you've found a previously placed queen along the left diag
+				// of the jth column so you cannot place queen r in the jth col
+				// of row r
+				return false;
+			}
+		}
 
-    void f(int r, int n, vector<string>& board, vector<vector<string>>& allBoard)
-    {
-        //base case
+		// you can place Qr in the jth column of row r
 
-        if(r==n)
-        { // if you build a valid configuration
-            allBoard.push_back(board);
-            return;
-        }
+		return true;
 
-        //recursive case
+	}
 
-        // f(r) = takes decision for Qr to Qn-1
+	void f(int r, int n, vector<string>& board,
+	       vector<vector<string>>& allBoards) {
 
-        // decide for Qr
+		// base case
 
-        for(int j = 0; j < n; j++)
-        {
-            if(isValid(r,n,board,j))
-            {
-                board[r][j] = 'Q';
-                f(r+1,n,board,allBoard);
-                board[r][j] = '.';
-            }
-        }
-    }
+		if (r == n) { // you've built a valid configuration, so track it
+			allBoards.push_back(board);
+			return;
+		}
 
-    vector<vector<string>> solveNQueens(int n) {
+		// recursive case
 
-        vector<vector<string>> allBoards;
+		// f(r) = take decisions for Qr to Qn-1
 
-        vector<string> board;
+		// decide for Qr
 
-        for(int i = 1; i <= n; i++)
-        {
-            string row(n,'.');
-            board.push_back(row);
-        }
+		for (int j = 0; j < n; j++) {
 
-        f(0,n,board,allBoards);
+			if (isvalid(r, n, board, j)) {
 
-        return allBoards;
-        
-    }
+				board[r][j] = 'Q';
+				f(r + 1, n, board, allBoards);
+				board[r][j] = '.';
+
+			}
+
+		}
+
+	}
+
+	vector<vector<string>> solveNQueens(int n) {
+
+		vector<vector<string>> allBoards;
+
+		vector<string> board;
+		for (int i = 1; i <= n; i++) {
+			string row(n, '.');
+			board.push_back(row);
+		}
+
+		f(0, n, board, allBoards);
+
+		return allBoards;
+
+	}
 };
 
+int main() {
 
-int main()
-{
-    Solution obj;
+	int n;
+	cin >> n;
 
-    int n = 4;
+	Solution s;
 
-    for(vector<string> a: obj.solveNQueens(n))
-    {
-        for(string b: a)
-        {
-            cout << b;
-            cout << endl;
-        }
+	vector<vector<string>> allBoards = s.solveNQueens(n);
 
-        cout << endl;
-    }
+	for (vector<string> board : allBoards) {
+		for (string row : board) {
+			cout << row << endl;
+		}
+		cout << endl;
+	}
+
+	return 0;
 }
